ff815cef1817c907b710ecafa3eff679481c8aac
CoeJoder <CoeJoder@users.noreply.github.com>
2020-09-29 19:00:11 -0700

    Create addStylesheet()

--- userscriptUtils.js
+++ userscriptUtils.js
@@ -6,19 +6,19 @@
 function UserscriptUtils() {
 	var that = this;
 
-	/**
-	 * Creates a new <style> with the given text and adds it to the <head>.
-	 *
-	 * Usage example:
-	 *
-     *     utils.addCss(`
-     *          p {
-     *    		    height: 50px;
-     *    	    }
-     *     `);
-	 * 
-	 * @param {string} css - The CSS rules to add.
-	 */
+    /**
+     * Add a new <style> with the given text to the <head>.
+     *
+     * Usage example:
+     *
+     * utils.addCss(`
+     *     p {
+     *         height: 50px;
+     *     }
+     * `);
+     *
+     * @param {string} css - The CSS rules to add.
+     */
     UserscriptUtils.prototype.addCss = function(css) {
         var style = document.createElement('style');
         style.type ='text/css';
@@ -31,26 +31,36 @@ function UserscriptUtils() {
         document.getElementsByTagName('head')[0].appendChild(style);
     }
 
-	/**
-	 * Detects and handles AJAXed content.
-	 *
-	 * Usage example:
-	 *
-	 *     function callback(domElement) {
-	 *         domElement.innerHTML = "This text inserted by waitForKeyElements().";
-	 *     }
-	 * 
-	 *     utils.waitForKeyElements("div.comments", callback);
-	 *     // or
-	 *     utils.waitForKeyElements(selectorFunction, callback);
-	 *
-	 * @param {(string|function)} selectorOrFunction - The selector string or function.
-	 * @param {function}          callback           - The callback function; takes a single DOM element as parameter.
-	 *                                                 If returns true, element will be processed again on subsequent iterations.
-	 * @param {boolean}           [waitOnce=true]    - Whether to stop after the first elements are found.
-	 * @param {number}            [interval=300]     - The time (ms) to wait between iterations.
-	 * @param {number}            [maxIntervals=-1]  - The max number of intervals to run (negative number for unlimited).
-	 */
+    /**
+     * Add a new <link> stylesheet at the given URL to the <head>.
+     */
+    UserscriptUtils.prototype.addStylesheet = function(href) {
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = href;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
+    /**
+     * Detect and handle AJAXed content.
+     *
+     * Usage example:
+     *
+     * function callback(domElement) {
+     *     domElement.innerHTML = "This text inserted by waitForKeyElements().";
+     * }
+     *
+     * utils.waitForKeyElements("div.comments", callback);
+     * // or
+     * utils.waitForKeyElements(selectorFunction, callback);
+     *
+     * @param {(string|function)} selectorOrFunction - The selector string or function.
+     * @param {function}          callback           - The callback function; takes a single DOM element as parameter.
+     *                                                 If returns true, element will be processed again on subsequent iterations.
+     * @param {boolean}           [waitOnce=true]    - Whether to stop after the first elements are found.
+     * @param {number}            [interval=300]     - The time (ms) to wait between iterations.
+     * @param {number}            [maxIntervals=-1]  - The max number of intervals to run (negative number for unlimited).
+     */
 	UserscriptUtils.prototype.waitForKeyElements = function(selectorOrFunction, callback, waitOnce, interval, maxIntervals) {
 		if (typeof waitOnce === "undefined") {
 			waitOnce = true;
